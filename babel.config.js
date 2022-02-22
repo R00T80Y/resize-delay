d3b07c124e0ac9eb4a8ce13d41abdb95ec133512
R00T80Y <R00T80Y@gmail.com>
2022-02-22 19:02:38 +0300

    ESLint for <babel.config.js>

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,14 @@
-const PRODUCTION_MODE = process.env.NODE_ENV === "production"
+const PRODUCTION_MODE = process.env.NODE_ENV === 'production';
 
 module.exports = {
   presets: [
     [
-      "@babel/preset-env",
+      '@babel/preset-env',
       {
-        "debug": PRODUCTION_MODE ? true : false,
-        "modules": false,
-        "loose": true
+        debug: !!PRODUCTION_MODE,
+        modules: false,
+        loose: true
       }
     ]
   ]
-}
+};
